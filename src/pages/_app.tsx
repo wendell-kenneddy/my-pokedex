31f562386db30d68e90861ce7243e47aebfb9863
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-11-18 16:10:24 -0300

    add page loading indicator function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+import NProgress from 'nprogress';
+import '../../public/nprogress.css';
 
 import { Navbar } from '../components/Navbar';
 import { theme } from '../theme';
 
 import { Box, ChakraProvider } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', NProgress.start);
+    router.events.on('routeChangeComplete', NProgress.done);
+    router.events.on('routeChangeError', NProgress.done);
+
+    return () => {
+      router.events.off('routeChangeStart', NProgress.start);
+      router.events.off('routeChangeComplete', NProgress.done);
+      router.events.off('routeChangeError', NProgress.done);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
